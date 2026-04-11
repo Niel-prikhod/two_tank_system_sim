530f620f0688d14a32f8ea99faff8b1d1101ff85
Niel <dyprih@gmail.com>
2026-04-11 15:31:04 +0200

    add script to create plot images

---
+++ run_model.m
@@ -0,0 +1,47 @@
+% simulation
+model = 'TwoTankModel';
+if bdIsLoaded(model)
+	close_system(model, 0);
+end
+open_system(model);
+simOut = sim(model, 'StopTime', '6000', 'SaveTime', 'on');
+
+% extract data from simulation
+t = simOut.tout;
+h1_data = simOut.h1;
+h2_data = simOut.h2;
+q12_data = simOut.Q12;
+q2_data = simOut.Q;
+close_system(model, 0);
+
+%plot data
+figure_1 = figure('Visible', 'off');
+plot(t, h2_data.Data, 'b-', 'LineWidth', 1.5); hold on;
+plot(t, h1_data.Data, 'g-', 'LineWidth', 1.5); 
+xlabel('Time (s)');
+ylabel('Water level (m)');
+title('Water level in both tanks');
+yline(0.816, 'r--', 'Steady state');
+legend({'h_1','h_2'},'Location','best');
+grid on;
+
+exportgraphics(figure_1, 'tank_levels.png', 'Resolution', 300);
+close(figure_1);
+ylim([0 0.85]);
+
+figure_2 = figure('Visible', 'off');
+plot(t, q12_data.Data, 'm-', 'LineWidth', 1.5); hold on;
+plot(t, q2_data.Data, 'c-', 'LineWidth', 1.5); 
+xlabel('Time (s)');
+ylabel('Flow rate (m^3/s)');
+title('Water level in both tanks');
+yline(10^-2, 'r--', 'Steady state');
+legend({'Q','Q_{12}'},'Location','best');
+grid on;
+ylim([0 0.012]);
+ax = gca;                                 
+handleax.YAxis.Exponent = 0;                    
+labelax.YAxis.TickLabelFormat = '%0.2e';
+
+exportgraphics(figure_2, 'flows.png', 'Resolution', 300);
+close(figure_2);
