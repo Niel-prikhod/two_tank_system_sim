b6c7c4efe334838c7d80500fcbc518522c8f4ede
Niel <dyprih@gmail.com>
2026-05-20 15:00:45 +0200

    remake signal generator, use another generation method - randomize a few control points and then interpolate

--- task_6/signal_gen.m
+++ task_6/signal_gen.m
@@ -1,12 +1,14 @@
 function out = signal_gen(offset, max_time, sample_time, disturbance)
 	rand_seed = 42;
     rng(rand_seed);
+	change_interval = 600;
+	t_ctrl = (0:change_interval:max_time)';
 	t = (0:sample_time:max_time)';
-	base_signal = ones(length(t), 1) * offset;
+	n_ctrl = length(t_ctrl);
+	control_points = ((2*rand(n_ctrl, 1) - 1) * (disturbance / 100) + 1) * offset;
+	disturbed_signal = interp1(t_ctrl, control_points, t, 'pchip');
 	min_limit = 0;
 	max_limit = 2 * offset;
-	disturbance_factor = 1 + (2*rand(length(t), 1) - 1) * (disturbance / 100);
-	disturbed_signal = base_signal .* disturbance_factor;
 	disturbed_signal = max(disturbed_signal, min_limit);   
 	disturbed_signal = min(disturbed_signal, max_limit);  
 	out = [t, disturbed_signal];
